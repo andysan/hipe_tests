5f8e7b0f440bd4a67fbdedc84cbc68f5f1176d1e
Kostis Sagonas <kostis@cs.ntua.gr>
2001-06-10 14:47:18 +0000

    Added a test for fnegate.

--- basic_tests/simpl_fl.erl
+++ basic_tests/simpl_fl.erl
@@ -8,10 +8,14 @@
 -export([test/0,compile/1]).
 
 test() ->
-    mult(2.5617).
+    {mult(2.5617),negate(6.023e23)}.
 
 mult(X) ->
     3.1415 * X.
+
+%% this tests the translation of the fnegate BEAM instruction.
+negate(X) ->
+    - (X + 0.0).
 
 compile(Flags) ->
     hipe:c(?MODULE,Flags).
