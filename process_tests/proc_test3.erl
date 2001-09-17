6972c9575db31c0012f1440927284106300141a5
Mikael Pettersson <mikpe@it.uu.se>
2001-09-17 15:35:55 +0000

    added exit/2 test

--- process_tests/proc_test3.erl
+++ process_tests/proc_test3.erl
@@ -5,34 +5,31 @@
 %%  Purpose  :  Checks correct exit of processes.
 %%  History  :  * 2001-08-13 Kostis Sagonas (kostis@csd.uu.se): Created.
 %% CVS:
-%%    $Author: kostis $
-%%    $Date: 2001/08/20 12:34:33 $
-%%    $Revision: 1.1 $
+%%    $Author: mikpe $
+%%    $Date: 2001/09/17 15:35:55 $
+%%    $Revision: 1.2 $
 %% ====================================================================
 
 -module(proc_test3).
--export([test/0,compile/1]). %,exit_2_catch/0]).
+-export([test/0,compile/1]).
 
 test() ->
-    my_exit(correctly).
+    exit_2_catch().
     
 compile(Flags) ->
     hipe:c(?MODULE,Flags).
 
-my_exit(Reason) ->
-    exit(self(),Reason).
+exit_2_catch() ->
+    process_flag(trap_exit,true),
+    Pid = fun_spawn(fun() -> catch exit(self(),die) end),
+    receive
+	{'EXIT',Pid,normal} ->
+	    {error,catch_worked};
+	{'EXIT',Pid,die} ->
+	    ok;
+	Other ->
+            {error,{other_message,Other}}
+    end.
 
-%exit_2_catch() ->
-%    process_flag(trap_exit,true),
-%    Pid = fun_spawn(fun() -> catch exit(self(),die) end),
-%    receive
-%	{'EXIT',Pid,normal} ->
-%	    {error,catch_worked};
-%	{'EXIT',Pid,die} ->
-%	    ok;
-%	Other ->
-%            {error,{other_message,Other}}
-%    end.
-
-%fun_spawn(Fun) ->
-%    spawn_link(erlang, apply, [Fun,[]]).
+fun_spawn(Fun) ->
+    spawn_link(erlang, apply, [Fun,[]]).
