f24cd7581d53645c3aee56891675919705880f27
Kostis Sagonas <kostis@cs.ntua.gr>
2001-09-20 09:59:17 +0000

    *** empty log message ***

--- process_tests/proc_test5.erl
+++ process_tests/proc_test5.erl
@@ -6,8 +6,8 @@
 %%  History  :  * 2001-09-19 Kostis Sagonas (kostis@csd.uu.se): Created.
 %% CVS:
 %%    $Author: kostis $
-%%    $Date: 2001/09/19 10:28:37 $
-%%    $Revision: 1.1 $
+%%    $Date: 2001/09/20 09:59:17 $
+%%    $Revision: 1.2 $
 %% ====================================================================
 
 -module(proc_test5).
@@ -19,15 +19,24 @@ test() ->
 compile(Flags) ->
     hipe:c(?MODULE,Flags).
 
+%%
+%% The case statement is used so as to make the code re-entrant.
+%%
 t_process_info() ->
-    %% [] = process_info(self(), registered_name),
-    %% register(my_name, self()),
-    %% {registered_name, my_name} = process_info(self(), registered_name),
-    %% {status, running} = process_info(self(), status),
-    {current_function, {?MODULE,t_process_info,0}} =
-        process_info(self(), current_function),
-    %% Gleader = group_leader(),
-    %% {group_leader, Gleader} = process_info(self(), group_leader),
-    %% {'EXIT',{badarg,Info}} = (catch process_info('not_a_pid')),
+    case process_info(self(), registered_name) of
+	[] ->
+	    register(my_name, self());
+	{registered_name,my_name} ->
+	    ok;
+        Other ->
+	    exit(Other)
+    end,
+    {registered_name, my_name} = process_info(self(), registered_name),
+    {status, running} = process_info(self(), status),
+    %% {current_function, {?MODULE,t_process_info,0}} =
+    %%     process_info(self(), current_function),
+    Gleader = group_leader(),
+    {group_leader, Gleader} = process_info(self(), group_leader),
+    {'EXIT',{badarg,Info}} = (catch process_info('not_a_pid')),
     ok.
 
