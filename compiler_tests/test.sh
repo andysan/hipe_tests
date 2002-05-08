abc43618f742af535e7d427f0dee9a322b6f1d17
Kostis Sagonas <kostis@cs.ntua.gr>
2002-05-08 10:20:17 +0000

    *** empty log message ***

--- compiler_tests/test.sh
+++ compiler_tests/test.sh
@@ -18,9 +18,7 @@ testfiles="hc?.erl"
 
 ix_exec ()
 {
-  {
-    echo echo "ctest:start\("$2","$3","$4"\). halt\(\)." \| $1 $5 -pa . 
-  } | sh
+    echo "ctest:start("$2","$3","$4"). halt()." \| $1 $5 -pa . 
 }
 
 ## NOTE: this does not use the same test.erl file as the other test dirs.
