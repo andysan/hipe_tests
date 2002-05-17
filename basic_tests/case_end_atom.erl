66724ee46c06a1ec090e9e1067bb703f9823056b
Kostis Sagonas <kostis@cs.ntua.gr>
2002-05-17 15:30:59 +0000

    *** empty log message ***

---
+++ basic_tests/case_end_atom.erl
@@ -0,0 +1,21 @@
+-module(case_end_atom).
+-export([test/0,compile/1]).
+-compile([inline]).	%% necessary for this test
+
+%%
+%% Tests whether the translation of a case_end instruction works
+%% when an exception (no matching case pattern) is to be raised.
+%%
+
+test() ->
+  {'EXIT',{{case_clause,some_atom},[_|_]}} = (catch test_case_stm_inlining()),
+  ok.
+
+test_case_stm_inlining() ->
+  case some_atom of
+    another_atom -> strange_result
+  end.
+
+compile(O) ->
+  hipe:c(?MODULE,O).
+
--- basic_tests/test.sh
+++ basic_tests/test.sh
@@ -14,7 +14,8 @@ ERL_FLAGS=$3
 testfiles="bif01.erl exception01.erl exception02.erl fun??.erl guard01.erl \
 	   simpl_*.erl tuple.erl small.erl length.erl catch*.erl \
 	   hash_test.erl random_test.erl test??.erl mode_errors.erl \
-	   switch_mix.erl var_tuple.erl int_overfl.erl inline_match.erl"
+	   switch_mix.erl var_tuple.erl int_overfl.erl inline_match.erl \
+	   case_end_atom.erl"
 
 #============================================================================
 
