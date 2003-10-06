3d83904266bd8a5f11e1d8add6126557b303b9b2
Kostis Sagonas <kostis@cs.ntua.gr>
2003-10-06 14:57:47 +0000

    Added one more function & did some cleanups

--- basic_tests/fp_ebb.erl
+++ basic_tests/fp_ebb.erl
@@ -1,22 +1,32 @@
 %%%-------------------------------------------------------------------
 %%% File    : fp_ebb.erl
 %%% Author  : Tobias Lindahl <tobiasl@csd.uu.se>
-%%% Description : Test the construction ov overlapping fp extended 
+%%% Description : Test the construction of overlapping fp extended 
 %%%               basic blocks where BEAM has constructed one and 
 %%%               hipe_icode_fp constructs the other one.
 %%%
-%%% Created : 18 Aug 2003 by Tobias Lindahl <tobias@dhcp-12-248.it.uu.se>
+%%% Created : 18 Aug 2003 by Tobias Lindahl <tobias@it.uu.se>
 %%%-------------------------------------------------------------------
+
 -module(fp_ebb).
 
 -export([test/0,compile/1]).
+-compile(export_all).
 
 test()->
-  1.0 == foo(2*math:pi()),
+  1.0 = foo(2*math:pi()),
+  1.0 = bar(2*math:pi()),
   ok.
 
 foo(X) ->
   X / (2 * math:pi()).
+
+bar(X) ->
+  F = 2.0,
+  case F < 3.0 of
+    true -> (X * F) / ((2 * F) * math:pi());
+    false -> weird
+  end.
 
 compile(O) ->
   hipe:c(?MODULE,O).
