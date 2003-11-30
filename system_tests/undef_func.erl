ac6249abc4911d7f945d426ab6e2244514e0f185
Kostis Sagonas <kostis@cs.ntua.gr>
2003-11-30 23:40:20 +0000

    Added test for unused local functions.

--- system_tests/undef_func.erl
+++ system_tests/undef_func.erl
@@ -1,19 +1,5 @@
-%% ``The contents of this file are subject to the Erlang Public License,
-%% Version 1.1, (the "License"); you may not use this file except in
-%% compliance with the License. You should have received a copy of the
-%% Erlang Public License along with this software. If not, it can be
-%% retrieved via the world wide web at http://www.erlang.org/.
 %% 
-%% Software distributed under the License is distributed on an "AS IS"
-%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
-%% the License for the specific language governing rights and limitations
-%% under the License.
-%% 
-%% The Initial Developer of the Original Code is Ericsson Utvecklings AB.
-%% Portions created by Ericsson are Copyright 1999, Ericsson Utvecklings
-%% AB. All Rights Reserved.''
-%% 
-%%     $Id: undef_func.erl,v 1.1 2003/07/01 13:28:45 richardc Exp $
+%%     $Id: undef_func.erl,v 1.2 2003/11/30 23:40:20 kostis Exp $
 %%
 
 -module(undef_func).
@@ -29,6 +15,7 @@ test() ->
     Path = [Root ++ D || D <- ["/lib/kernel/ebin",
 			       "/lib/stdlib/ebin",
 			       "/lib/compiler/ebin",
+			       %% gs is needed for the hipe_tool
 			       "/lib/gs/ebin"]],
     Server = hipe_testsuite,
     xref:start(Server),
@@ -36,17 +23,29 @@ test() ->
     xref:set_library_path(Server, Path),
     {ok,_App} = xref:add_application(Server, HiPE),
     {ok,Undef} = xref:analyze(Server, undefined_function_calls),
+    {ok,UnusedLocals} = xref:analyze(Server, locals_not_used),
     catch xref:stop(Server),
-    case Undef of
-	[] -> no_undefined_functions;
-	_ ->
-	    lists:foreach(fun ({MFA1,MFA2}) ->
-				  io:format("~s calls undefined ~s\n",
-					    [format_mfa(MFA1),
-					     format_mfa(MFA2)])
-			  end, Undef),
-	    {undefined_functions_in_hipe, Undef}
-    end.
+    U1 = case Undef of
+	     [] -> no_undefined_functions;
+	     _ ->
+		 lists:foreach(fun ({MFA1,MFA2}) ->
+				       io:format("~s calls undefined ~s\n",
+						 [format_mfa(MFA1),
+						  format_mfa(MFA2)])
+			       end, Undef),
+		 {undefined_functions_in_hipe,Undef}
+	 end,
+    U2 = case UnusedLocals of
+	     [] -> no_unused_local_functions;
+	     _ ->
+		 io:format("~w\n", [UnusedLocals]),
+		 lists:foreach(fun (MFA) ->
+				       io:format("unused: ~s\n",
+						 [format_mfa(MFA)])
+			       end, UnusedLocals),
+		 {unused_locals_in_hipe,UnusedLocals}
+	 end,
+    {U1,U2}.
 
 %%% "(XC - UC) || (XU - X - B)"
 
