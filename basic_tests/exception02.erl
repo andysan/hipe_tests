0fdb9e3cf9fb0bb55f6c782b81a6da1d7e611478
Kostis Sagonas <kostis@cs.ntua.gr>
2004-02-06 20:11:33 +0000

    Fixed identation.

--- basic_tests/exception02.erl
+++ basic_tests/exception02.erl
@@ -1,3 +1,4 @@
+%% -*- erlang-indent-level: 2 -*-
 %% ====================================================================
 %% Test module for the HiPE test suite.  Taken from fun_SUITE.erl
 %%
@@ -6,8 +7,8 @@
 %%  History  :  * 2001-09-20 Kostis Sagonas (kostis@csd.uu.se): Created.
 %% CVS:
 %%    $Author: kostis $
-%%    $Date: 2003/10/08 15:39:26 $
-%%    $Revision: 1.3 $
+%%    $Date: 2004/02/06 20:11:33 $
+%%    $Revision: 1.4 $
 %% ====================================================================
 
 -module(exception02).
@@ -20,25 +21,25 @@ compile(Flags) ->
   hipe:c(?MODULE,Flags).
 
 bad_fun_call() ->
-    bad_call_fc(42),
-    bad_call_fc(xx),
-    bad_call_fc({}),
-    bad_call_fc({1}),
-    bad_call_fc({1,2,3}),
-    bad_call_fc({1,2,3}),
-    bad_call_fc({1,2,3,4}),
-    bad_call_fc({1,2,3,4,5,6}),
-    bad_call_fc({1,2,3,4,5}),
-    bad_call_fc({1,2}),
-    ok.
+  bad_call_fc(42),
+  bad_call_fc(xx),
+  bad_call_fc({}),
+  bad_call_fc({1}),
+  bad_call_fc({1,2,3}),
+  bad_call_fc({1,2,3}),
+  bad_call_fc({1,2,3,4}),
+  bad_call_fc({1,2,3,4,5,6}),
+  bad_call_fc({1,2,3,4,5}),
+  bad_call_fc({1,2}),
+  ok.
 
 bad_call_fc(Fun) ->
-    Args = [some,stupid,args],
-    Res = (catch Fun(Fun(Args))),
-    case Res of
-        {'EXIT',{{badfun,Fun},_Where}} ->
-            ok; %% = io:format("~p(~p) -> ~p\n", [Fun,Args,Res]);
-        Other ->
-            io:format("~p(~p) -> ~p\n", [Fun,Args,Res]),
-            exit({bad_result,Other})
-    end.
+  Args = [some,stupid,args],
+  Res = (catch Fun(Fun(Args))),
+  case Res of
+    {'EXIT',{{badfun,Fun},_Where}} ->
+      ok; %% = io:format("~p(~p) -> ~p\n", [Fun,Args,Res]);
+    Other ->
+      io:format("~p(~p) -> ~p\n", [Fun,Args,Res]),
+      exit({bad_result,Other})
+  end.
