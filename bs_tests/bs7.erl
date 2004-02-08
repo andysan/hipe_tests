cfdb8c09d17b1385e3670a701bc28b519a0fcd7e
Kostis Sagonas <kostis@cs.ntua.gr>
2004-02-08 20:50:56 +0000

    Cleanup changes.

--- bs_tests/bs7.erl
+++ bs_tests/bs7.erl
@@ -1,31 +1,20 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Copyright (c) 2001 by Erik Johansson.  All Rights Reserved 
-%% Time-stamp: <01/06/13 11:52:20 happi>
-%% ====================================================================
-%%  Filename : 	bs.erl
-%%  Module   :	bs
+%%  Filename : 	bs7.erl
 %%  Purpose  :  
 %%  Notes    : 
-%%  History  :	* 2001-04-10 Erik Johansson (happi@csd.uu.se): 
-%%               Created.
 %%  CVS      :
 %%              $Author: kostis $
-%%              $Date: 2002/05/08 07:33:41 $
-%%              $Revision: 1.2 $
-%% ====================================================================
-%%  Exports  :
-%%
+%%              $Date: 2004/02/08 20:50:56 $
+%%              $Revision: 1.3 $
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -module(bs7).
 -export([test/0,compile/1]).
 
--define(FAIL(Expr), {'EXIT',{badarg,_}} = (catch Expr)).
-
 compile(O) ->
   hipe:c(?MODULE,O).
 
-test()->
+test() ->
   1 = in_guard(<<16#74ad:16>>, 16#e95, 5),
   2 = in_guard(<<16#3A,16#F7,"hello">>, 16#3AF7, <<"hello">>),
   3 = in_guard(<<16#FBCD:14,3.1415/float,3:2>>, 16#FBCD, 3.1415),
@@ -35,7 +24,6 @@ test()->
   nope = in_guard(<<4,5>>, 1, 2.71),
   nope = in_guard(<<4,5>>, 1, <<12,13>>),
   ok.
-
 
 in_guard(Bin, A, B) when <<A:13,B:3>> == Bin -> 1;
 in_guard(Bin, A, B) when <<A:16,B/binary>> == Bin -> 2;
