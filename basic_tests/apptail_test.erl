123632bf3f231faed05b686a8a471adca28ed7f9
Kostis Sagonas <kostis@cs.ntua.gr>
2004-02-17 09:29:07 +0000

    Made it work with naive regalloc too.

--- basic_tests/apptail_test.erl
+++ basic_tests/apptail_test.erl
@@ -1,9 +1,14 @@
-%%% $Id: apptail_test.erl,v 1.1 2003/11/12 21:08:00 mikpe Exp $
+%%% $Id: apptail_test.erl,v 1.2 2004/02/17 09:29:07 kostis Exp $
 %%% Check that apply is tail-recursive.
+%%
+%%% NOTE: Increased the SIZE_INCREASE from 20 to 30 so that it
+%%%	  can also be tested with the naive register allocator.
 
 -module(apptail_test).
 -export([test/0, compile/1]).
 -export([app0/2]).
+
+-define(SIZE_INCREASE, 30).
 
 compile(Flags) ->
     hipe:c(?MODULE, Flags).
@@ -17,9 +22,9 @@ start(N) ->
 
 app0(0, Size0) ->
     Size1 = hipe_bifs:nstack_used_size(),
-    %io:format("Size0 ~w, Size1 ~w\n", [Size0, Size1]),
-    if Size1 > Size0+20 ->
-	    exit(?MODULE);
+    %% io:format("Size0 ~w, Size1 ~w\n", [Size0, Size1]),
+    if Size1 > Size0+?SIZE_INCREASE ->
+	 exit(?MODULE);
        true -> []
     end;
 app0(N, Size) ->
