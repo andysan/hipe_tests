6c1f308bc9d27bb33e1b939c46db1da871a9b96c
Kostis Sagonas <kostis@cs.ntua.gr>
2004-02-18 23:14:56 +0000

    Added a test which exercises calling native funs from interpreted code.

--- basic_tests/test.sh
+++ basic_tests/test.sh
@@ -15,7 +15,7 @@ testfiles="bif01.erl exception01.erl exception02.erl fun??.erl guard01.erl \
 	   simpl_*.erl tuple.erl small.erl length.erl catch*.erl \
 	   *_test.erl test??.erl mode_errors.erl hello_world.erl \
 	   switch_mix.erl var_tuple.erl int_overfl.erl inline_match.erl \
-	   case_end_atom.erl ets_bug.erl *_bug_?.erl fp_ebb.erl comp_tmout.erl"
+	   case_end_atom.erl *_bug.erl *_bug_?.erl fp_ebb.erl comp_tmout.erl"
 #testfiles="$testfiles try_expr.erl"
 
 testdir="basic_tests"
