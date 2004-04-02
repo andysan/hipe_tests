6f223247026ab920aed1f186530a05727bf082b0
Mikael Pettersson <mikpe@it.uu.se>
2004-04-02 10:22:45 +0000

    made apptail_test more robust for PowerPC with ra_dummy

--- basic_tests/apptail_test.erl
+++ basic_tests/apptail_test.erl
@@ -1,4 +1,4 @@
-%%% $Id: apptail_test.erl,v 1.2 2004/02/17 09:29:07 kostis Exp $
+%%% $Id: apptail_test.erl,v 1.3 2004/04/02 10:22:45 mikpe Exp $
 %%% Check that apply is tail-recursive.
 %%
 %%% NOTE: Increased the SIZE_INCREASE from 20 to 30 so that it
@@ -14,18 +14,18 @@ compile(Flags) ->
     hipe:c(?MODULE, Flags).
 
 test() ->
-    start(400),
+    Inc = start(400),
+    %% io:format("Inc ~w\n", [Inc]),
+    if Inc > ?SIZE_INCREASE ->
+	 exit(?MODULE);
+       true -> []
+    end,
     ok.
 
 start(N) ->
     app0(N, hipe_bifs:nstack_used_size()).
 
 app0(0, Size0) ->
-    Size1 = hipe_bifs:nstack_used_size(),
-    %% io:format("Size0 ~w, Size1 ~w\n", [Size0, Size1]),
-    if Size1 > Size0+?SIZE_INCREASE ->
-	 exit(?MODULE);
-       true -> []
-    end;
+    hipe_bifs:nstack_used_size() - Size0;
 app0(N, Size) ->
     apply(?MODULE, app0, [N-1, Size]).
