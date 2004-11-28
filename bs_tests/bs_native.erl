8486e29a0cc22359f3f456142f9046874ef06041
Kostis Sagonas <kostis@cs.ntua.gr>
2004-11-28 11:18:16 +0000

    *** empty log message ***

---
+++ bs_tests/bs_native.erl
@@ -0,0 +1,25 @@
+%% -*- erlang-indent-level: 2 -*-
+%%-------------------------------------------------------------------
+%% File        : bs_native.erl
+%% Author      : Kostis Sagonas
+%% Description : Test sent by Bjorn Gustavsson to report a bug in the
+%%               handling of the 'native' endian specifier on Mac OS X.
+%%		 Actually, the 'native' specifier was probably treated
+%%		 incorrectly on all plarforms.
+%% Created     : 28 Nov 2004
+%%-------------------------------------------------------------------
+
+-module(bs_native).
+-export([test/0, compile/1]).
+
+test() ->
+  BeamRes = vs_to_bin({1.0,2.0,3.0}),
+  hipe:c({?MODULE,vs_to_bin,1}, [inline_bs,o2]),
+  HipeRes = vs_to_bin({1.0,2.0,3.0}),
+  {BeamRes, HipeRes}.
+
+vs_to_bin({X,Y,Z}) ->
+  <<X:64/native-float,Y:64/native-float,Z:64/native-float>>.
+
+compile(Opts) ->
+  hipe:c(?MODULE, Opts).
