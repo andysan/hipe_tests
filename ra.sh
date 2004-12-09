f6dccf4a8005636aa88d222491b1faab338cf13a
Kostis Sagonas <kostis@cs.ntua.gr>
2004-12-09 16:42:18 +0000

    Added test of naive register allocator.

--- ra.sh
+++ ra.sh
@@ -5,7 +5,7 @@
 ## Author(s): Kostis Sagonas
 ## Purpose:   To test the HiPE system with all register allocators.
 ##
-## $Id: ra.sh,v 1.3 2004/10/16 15:23:30 richardc Exp $
+## $Id: ra.sh,v 1.4 2004/12/09 16:42:18 kostis Exp $
 ##====================================================================
 
 # Run from testsuite directory
@@ -19,6 +19,7 @@ else
     OTP_DIR=$1
 fi
 
+./testsuite.sh --comp_opts "[o2,\{regalloc,naive\}]" $OTP_DIR
 ./testsuite.sh --comp_opts "[o2]" "$OTP_DIR"	## this tests the default RA
 ./testsuite.sh --comp_opts "[o2,\{regalloc,linear_scan\}]" "$OTP_DIR"
 ./testsuite.sh --comp_opts "[o2,\{regalloc,graph_color\}]" "$OTP_DIR"
