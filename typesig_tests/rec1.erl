49e774e4ec1395be6febabfe1b45457a81c0c2cc
Kostis Sagonas <kostis@cs.ntua.gr>
2005-01-12 15:01:18 +0000

    *** empty log message ***

--- typesig_tests/rec1.erl
+++ typesig_tests/rec1.erl
@@ -6,10 +6,9 @@
 %%% Created : 21 Dec 2004 by Tobias Lindahl <tobiasl@it.uu.se>
 %%%-------------------------------------------------------------------
 -module(rec1).
+-export([traverse_linked_list/1]).
 
--compile(export_all).
-
-traverse_linked_list({Content, nil})->
+traverse_linked_list({Content, nil}) ->
   [Content];
 traverse_linked_list({Content, Next}) ->
   [Content] ++ traverse_linked_list(Next).
