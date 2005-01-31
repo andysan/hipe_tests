e104daac03baa32d8d2b3afa3f91dffd1975d030
Kostis Sagonas <kostis@cs.ntua.gr>
2005-01-31 17:57:05 +0000

    Added weird case of receive.

---
+++ typesig_tests/receive2.erl
@@ -0,0 +1,16 @@
+%%%-------------------------------------------------------------------
+%%% File    : receive2.erl
+%%% Author  : Kostis Sagonas <kostis@it.uu.se>
+%%% Description : A weird program to exercise the type analyzer.
+%%%
+%%% Created : 27 Jan 2005 by Kostis Sagonas <kostis@it.uu.se>
+%%%-------------------------------------------------------------------
+-module(receive2).
+
+-compile(export_all).
+
+t(Arg) ->
+  receive
+    {foo, X} = Arg when is_integer(X) -> {int, X};
+    Y = Arg when is_float(Y) -> {float, Y}
+  end.
