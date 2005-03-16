ee86861c6fe1a80ee6252f647f6eb595c286e758
Kostis Sagonas <kostis@cs.ntua.gr>
2005-03-16 15:54:45 +0000

    *** empty log message ***

--- typesig_tests/list1.erl
+++ typesig_tests/list1.erl
@@ -1,30 +1,39 @@
 %%%-------------------------------------------------------------------
 %%% File    : list1.erl
-%%% Author  : Tobias Lindahl <tobias@dhcp-12-242.it.uu.se>
+%%% Author  : Tobias Lindahl <tobias@it.uu.se>
 %%% Description : 
 %%%
-%%% Created :  3 Mar 2003 by Tobias Lindahl <tobias@dhcp-12-242.it.uu.se>
+%%% Created :  3 Mar 2003 by Tobias Lindahl <tobias@it.uu.se>
 %%%-------------------------------------------------------------------
 
 -module(list1).
+-export([doit/0, append/2, reverse/1, lists__reverse/1, reverse_acc/1]).
 
--export([doit/0]).
-
-doit()->
+doit() ->
   {len([1, 2, 3], 0)}.
 
-
-len([_|T], Acc)->
+len([_|T], Acc) ->
   len(T, Acc+1);
-len([], Acc) when is_integer(Acc)->
+len([], Acc) when is_integer(Acc) ->
   Acc.
+
+append([H|T], Z) ->
+   [H|append(T, Z)];
+append([], X) ->
+   X.
 
 reverse([H|T]) ->
   reverse(T) ++ [H];
 reverse([]) ->
   [].
 
-append([H|T], Z) ->
-   [H|append(T, Z)];
-append([], X) ->
-   X.
+lists__reverse(L) ->
+   lists:reverse(L).
+
+reverse_acc(L) ->
+   reverse_acc(L, []).
+
+reverse_acc([], Acc) -> % when length(Acc) >= 0 ->
+   Acc;
+reverse_acc([H|T], Acc) ->
+   reverse_acc(T, [H|Acc]).
