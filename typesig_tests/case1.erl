5681627b45f80e0a842d0ca31cea0e13cf6773bd
Kostis Sagonas <kostis@cs.ntua.gr>
2005-05-25 12:29:03 +0000

    Updated the file to have both local and exported functions.

--- typesig_tests/case1.erl
+++ typesig_tests/case1.erl
@@ -6,11 +6,18 @@
 %%% Created : 11 Aug 2004 by Tobias Lindahl <tobiasl@it.uu.se>
 %%%-------------------------------------------------------------------
 -module(case1).
+-export([t/0, t/1]).
 
--compile(export_all).
+t() ->
+  t_local(1).
 
-doit()->
-  t(1).
+t_local(X) ->
+  case {1, X} of
+    {1, 2} ->
+      ok;
+    {_, 3} -> X;
+    Foo -> Foo
+  end.
 
 t(X) ->
   case {1, X} of
