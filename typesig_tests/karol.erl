710e0ab69a65745dda90948b0570fc345ebaa6fe
Kostis Sagonas <kostis@cs.ntua.gr>
2005-05-26 09:46:09 +0000

    Karol's test.

--- typesig_tests/karol.erl
+++ typesig_tests/karol.erl
@@ -9,15 +9,12 @@
 expand_args_any(0) ->
   [];
 expand_args_any(X) when is_atom(X) ->
-  X = karol(),
+  X = karol,
   ["_"];
 expand_args_any(1) ->
   ["_"];
 expand_args_any(X) ->
   ["_"|expand_args_any(X-1)].
-
-karol() ->
-  karol.
 
 %% Function for which type analysis produces something relatively weird...
 three() ->
