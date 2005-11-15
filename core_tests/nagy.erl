eb4ed9e8157ff8b6ef6840d04502b9735e4cf8a5
Kostis Sagonas <kostis@cs.ntua.gr>
2005-11-15 15:41:21 +0000

    Added v3_core pattern matching bug.

--- core_tests/nagy.erl
+++ core_tests/nagy.erl
@@ -1,13 +1,27 @@
+%%--------------------------------------------------------------------------
+%% The BEAM compiler, in its Core Erlang passes, gets confused by the
+%% presence of new lines, in the middle of pattern matching operations.
+%%
+%% The following program used to spit out a warning when compiled
+%% by the R10B-8 BEAM compiler that a matching will fail.  Turns out
+%% there was some assumption that the representations of 'simple' things
+%% like constants will match, if they should match as patterns
+%%
+%% Reported by Peter Nagy (Ericsson, Hungary) and fixed by Richard Carlsson.
+%% Twst program by Kostis Sagonas.
+%%--------------------------------------------------------------------------
+
 -module(nagy).
 -export([test/0, compile/1]).
 
--record(rec, {field1,field2,field3,field4}).
+-record(rec, {f1,f2}).
 
 test() ->
-     [#rec{field1 = A, field2 = B}]
-   = [TRec] = [#rec{field1=a,field2=b}],
-     TRec1 = TRec#rec{field3 = something3, field4 = something4},
-     {TRec1,TRec,A,B}.
+     [#rec{f1 = A}]	%% do not remove this line break
+	 = [TRec] = [#rec{f1 = a}],
+     TRec1 = TRec#rec{f2 = b},
+     {{rec,a,b},{rec,a,undefined},a} = {TRec1,TRec,A},
+     ok.
 
 compile(Opts) ->
     hipe:c(?MODULE, [core|Opts]).
