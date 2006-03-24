bd9ce6492eef3dbe586e627a32726900bcad0bcf
Kostis Sagonas <kostis@cs.ntua.gr>
2006-03-24 10:08:41 +0000

    Cosmetic changes -- and one change to function error.

--- typesig_tests/unary_plus.erl
+++ typesig_tests/unary_plus.erl
@@ -1,13 +1,12 @@
-%%%-------------------------------------------------------------------
-%%% File    : unary_plus.erl
-%%% Author  : Tobias Lindahl <tobiasl@it.uu.se>
-%%% Description : 
-%%%
-%%% Created : 11 Aug 2004 by Tobias Lindahl <tobiasl@it.uu.se>
-%%%-------------------------------------------------------------------
+%%-------------------------------------------------------------------
+%% File        : unary_plus.erl
+%% Author      : Tobias Lindahl <tobiasl@it.uu.se>
+%% Description : Checks that the '+'/1 functions works as in R10B.
+%% Created     : 11 Aug 2005 by Tobias Lindahl <tobiasl@it.uu.se>
+%%-------------------------------------------------------------------
 -module(unary_plus).
 -export([t/1]).
--export([error/1]).
+-export([error/0]).
 
 t() ->
   t(3.14) + t(42).
@@ -15,5 +14,5 @@ t() ->
 t(X) ->
   +(X).
 
-error(_) ->
+error() ->
   +(gazonk).
