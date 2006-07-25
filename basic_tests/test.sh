f46d67b5156dab2492a20239de82f956cf04da1a
Mikael Pettersson <mikpe@it.uu.se>
2006-07-25 23:17:02 +0000

    enable try_expr1 and try_expr3 tests as they now work

--- basic_tests/test.sh
+++ basic_tests/test.sh
@@ -17,7 +17,7 @@ testfiles="bi*.erl exception01.erl exception02.erl fun??.erl guard*.erl \
 	   switch_mix.erl var_tuple.erl int_overfl.erl inline_match.erl \
 	   case_end_atom.erl *_bug*.erl fp_*.erl comp_tmout.erl \
 	   merged_catches.erl merged_const.erl unsafe_records.erl \
-	   is_function2.erl"
+	   is_function2.erl try_expr1.erl try_expr3.erl"
 #testfiles="$testfiles try_expr.erl"
 
 testdir="basic_tests"
