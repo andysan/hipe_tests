74c36ec988562dd3c7a16922af35ece30a4d121e
Kostis Sagonas <kostis@cs.ntua.gr>
2006-07-26 00:24:59 +0000

    Further simplification since now all tests in this directory work.

--- basic_tests/test.sh
+++ basic_tests/test.sh
@@ -11,14 +11,7 @@ ERL_FLAGS=$3
 
 #============================================================================
 
-testfiles="bi*.erl exception01.erl exception02.erl fun??.erl guard*.erl \
-	   simpl_*.erl tuple.erl small.erl length.erl catch*.erl \
-	   *_test.erl test??.erl mode_errors.erl hello_world.erl \
-	   switch_mix.erl var_tuple.erl int_overfl.erl inline_match.erl \
-	   case_end_atom.erl *_bug*.erl fp_*.erl comp_tmout.erl \
-	   merged_catches.erl merged_const.erl unsafe_records.erl \
-	   is_function2.erl try_expr1.erl try_expr3.erl"
-#testfiles="$testfiles try_expr.erl"
+testfiles="*.erl"
 
 testdir="basic_tests"
 
