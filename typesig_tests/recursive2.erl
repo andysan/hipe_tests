0ca14ccfe7bff1906a474ab00e6f37a60b9390fc
Kostis Sagonas <kostis@cs.ntua.gr>
2006-08-30 13:49:46 +0000

    Cleanup changes.

--- typesig_tests/recursive2.erl
+++ typesig_tests/recursive2.erl
@@ -1,8 +1,8 @@
 %% Program showing a bug in inferrence of refined success typings.
-%% Type propagation results in none() for the two collect functions.
+%% Type propagation resulted in none() for the two collect functions.
 
 -module(recursive2).
--export([collect/2,field_width/2,field_val/2]).
+-export([collect/2]).
 
 collect([$~|Fmt0], Args0) ->
     {C,Fmt1,Args1} = collect_cseq(Fmt0, Args0),
@@ -15,18 +15,6 @@ collect_cseq(Fmt0, Args0) ->
     {F,Ad,Fmt1,Args1} = field_width(Fmt0, Args0),
     {{F,Ad},Fmt1,Args1}.
 
-field_width([$-|Fmt0], Args0) ->
-    {F,Fmt,Args} = field_val(Fmt0, Args0),
-    width(-F, Fmt, Args);
-field_width(Fmt0, Args0) ->
-    {F,Fmt,Args} = field_val(Fmt0, Args0),
-    width(F, Fmt, Args).
-
-width(F, Fmt, Args) when F < 0 ->
-    {-F,left,Fmt,Args};
-width(F, Fmt, Args) when F >= 0 ->
-    {F,right,Fmt,Args}.
-
 field_val([$*|Fmt], [A|Args]) when is_integer(A) ->
     {A,Fmt,Args};
 field_val([C|Fmt], Args) when C >= $0, C =< $9 ->
@@ -34,8 +22,20 @@ field_val([C|Fmt], Args) when C >= $0, C =< $9 ->
 field_val(Fmt, Args) ->
     {tag,Fmt,Args}.
 
+field_width([$-|Fmt0], Args0) ->
+    {F,Fmt,Args} = field_val(Fmt0, Args0),
+    width(-F, Fmt, Args);
+field_width(Fmt0, Args0) ->
+    {F,Fmt,Args} = field_val(Fmt0, Args0),
+    width(F, Fmt, Args).
+
 value([C|Fmt], Args, F) when is_integer(C), C >= $0, C =< $9 ->
     value(Fmt, Args, 10*F + (C - $0));
-value(Fmt, Args, F) ->
+value(Fmt, Args, F) -> %% when is_integer(F) ->
     {F,Fmt,Args}.
+
+width(F, Fmt, Args) when F < 0 ->
+    {-F,left,Fmt,Args};
+width(F, Fmt, Args) when F >= 0 ->
+    {F,right,Fmt,Args}.
 
