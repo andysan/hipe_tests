5b8a1b3c9554943ab81b5bb5e73bc351d81a4976
Tobias Lindahl <tobiasl@it.uu.se>
2006-09-12 20:01:46 +0000

    Added []-test

--- typesig_tests/equal1.erl
+++ typesig_tests/equal1.erl
@@ -1,6 +1,11 @@
 -module(equal1).
--export([eq/1, exact_eq/1]).
+-export([eq1/1, exact_eq1/1, eq2/1, exact_eq2/1]).
 
-eq(B) when B == foo -> B.
+eq1(B) when B == foo -> B.
 
-exact_eq(B) when B =:= foo -> B.
+exact_eq1(B) when B =:= foo -> B.
+
+
+eq2(B) when B == [] -> B.
+
+exact_eq2(B) when B =:= [] -> B.
