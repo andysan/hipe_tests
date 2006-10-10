6256f4c2a48809f75c829d12c8ee1e8551122fd7
Per Gustafsson <Per.Gustafsson@it.uu.se>
2006-10-10 09:43:40 +0000

    adding a new test case for which the emulator fails. this testcase will work again as soon as we have import OTPs changes

---
+++ basic_tests/bsl_bug.erl
@@ -0,0 +1,28 @@
+%%%-------------------------------------------------------------------
+%%% File    : bsl_bug.erl
+%%% Author  : Per Gustafsson <pergu@it.uu.se>
+%%% Description : This causes a bug in the emulator to trigger
+%%%               as one of the arguments to bsl is not an integer
+%%%
+%%% Created : 28 Sep 2006 by Per Gustafsson <pergu@it.uu.se>
+%%%-------------------------------------------------------------------
+-module(bsl_bug).
+
+-export([test/0,compile/1]).
+
+compile(O) -> 
+  hipe:c(?MODULE,O).
+
+test() ->
+  {'EXIT',{'badarith',_}} = (catch (t1(0,pad,0))),
+  {'EXIT',{'badarith',_}} = (catch (t2(0,pad,0))),
+  ok.
+
+t1(_,X,_) ->
+  (1 bsl X) + 1.
+
+t2(_,X,_) ->
+  (X bsl 1) + 1.
+
+
+  
