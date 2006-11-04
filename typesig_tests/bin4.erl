03ec30d23dbef9f303d98c7cf12b86d8e6c7901f
Kostis Sagonas <kostis@cs.ntua.gr>
2006-11-04 22:18:44 +0000

    Changes to add something with ranges.

--- typesig_tests/bin4.erl
+++ typesig_tests/bin4.erl
@@ -1,11 +1,15 @@
 -module(bin4).
--export([leaf_to_bin/1]).
+-export([list_to_binlist/1]).
 
-leaf_to_bin(List) ->
-    leaf_to_bin(List, []).
+list_to_binlist(List) ->
+    {intlist_to_binlist(List, []), rnglist_to_binlist(List, [])}.
 
-leaf_to_bin([N | L], Acc) ->
-    leaf_to_bin(L, [<<N:32>> | Acc]);
-leaf_to_bin([], Acc) ->
+intlist_to_binlist([N | L], Acc) ->
+    intlist_to_binlist(L, [<<N:32>> | Acc]);
+intlist_to_binlist([], Acc) ->
     Acc.
 
+rnglist_to_binlist([N | L], Acc) when is_integer(N), 0 =< N, N =< 4294967295 ->
+    rnglist_to_binlist(L, [<<N:32>> | Acc]);
+rnglist_to_binlist([], Acc) ->
+    Acc.
