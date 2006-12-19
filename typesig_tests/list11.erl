60700a18b746f84726c30fe7996a14a5daeeac9e
Kostis Sagonas <kostis@cs.ntua.gr>
2006-12-19 23:43:31 +0000

    Added all files.

--- typesig_tests/list11.erl
+++ typesig_tests/list11.erl
@@ -1,17 +1,23 @@
 %%--------------------------------------------------------------
 %% Puzzling: Why aren't refined success typings strong enough to
 %% discover that the second argument of highest_label/2 is integer() ?
+%%
+%% Answer: Because the second parameter can be anything during
+%% the loop due to the call to label_name/1 and then just before
+%% the leaf call turn back to an integer.
 %%--------------------------------------------------------------
 -module(list11).
 -export([highest_label/1]).
+
+-include("list11.hrl").
 
 highest_label(Code) ->
   highest_label(Code, 0).
                                                                                 
 highest_label([I|Is], Max) ->
-  case foo:is_label(I) of
+  case is_label(I) of
     true ->
-      L = foo:label_name(I),
+      L = label_name(I),
       NewMax = if L > Max -> L; true -> Max end,
       highest_label(Is, NewMax);
     false ->
@@ -19,3 +25,8 @@ highest_label([I|Is], Max) ->
   end;
 highest_label([], Max) when is_integer(Max) ->
   Max.
+
+is_label(#label{}) -> true;
+is_label(_) -> false.
+
+label_name(#label{name=L}) -> L.
