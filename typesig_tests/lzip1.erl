e9185477b9c25211efe1f59203d8772c2d9d0553
Kostis Sagonas <kostis@cs.ntua.gr>
2007-01-25 19:21:22 +0000

    Added more test cases.

--- typesig_tests/lzip1.erl
+++ typesig_tests/lzip1.erl
@@ -2,7 +2,9 @@
 -export([zip2/0, zip3/0,
 	 zipwith3/0, zipwith3_nil/0,
 	 zipwith4/0, zipwith4_nil/0,
-	 unzip1/0]).
+	 unzip1_known_types/0, unzip1_unknown_types/1,
+	 unzip1_nil/0, unzip1_error/0,
+	 unzip3_known_types/0]).
 
 zip2() ->
   lists:zip([1,2], [a,b]).
@@ -26,5 +28,17 @@ zipwith4_nil() ->
   F = fun (X,Y,_) -> {X,gazonk,Y} end,
   lists:zipwith3(F, [], [], []).
 
-unzip1() ->
+unzip1_known_types() ->
   lists:unzip(zip2()).
+
+unzip1_unknown_types(List) ->
+  lists:unzip(List).
+
+unzip1_nil() ->
+  lists:unzip([]).
+
+unzip1_error() ->
+  lists:unzip(zip3()).
+
+unzip3_known_types() ->
+  lists:unzip3([{a,b,c}]).
