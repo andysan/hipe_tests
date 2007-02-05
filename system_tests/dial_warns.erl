3a807b727590262f1dec1c90ef45f9b99d508769
Per Gustafsson <Per.Gustafsson@it.uu.se>
2007-02-05 12:34:12 +0000

    Tobias had broken this test

--- system_tests/dial_warns.erl
+++ system_tests/dial_warns.erl
@@ -4,7 +4,7 @@
 %% Purpose: To run Dialyzer on the HiPE application and test that
 %%          there are no discrepancies that it identifies.
 %%
-%% $Id: dial_warns.erl,v 1.11 2006/03/21 22:17:42 kostis Exp $
+%% $Id: dial_warns.erl,v 1.12 2007/02/05 12:34:12 pergu Exp $
 %%
 
 -module(dial_warns).
@@ -30,7 +30,7 @@ test() ->
     %% the call to dialyzer changes the current working directory to
     %% the ebin dir of the application which is analyzed
     true = os:putenv("DIALYZER_USE_CALLGRAPH", "true"),
-    0 = dialyzer:run([{from,byte_code},{files,[HiPE_Dir]},{init_plt,InitPlt}]),
+    {ok,[]} = dialyzer:run([{from,byte_code},{files,[HiPE_Dir]},{init_plt,InitPlt}]),
     %% so change the current working dir back to what we started from
     file:set_cwd(CWD),
     ok.
