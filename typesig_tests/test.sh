bb9d5b5af999a08a25a2f3170c452f16cf5edaa4
Tobias Lindahl <tobiasl@it.uu.se>
2007-04-13 14:17:25 +0000

    *** empty log message ***

--- typesig_tests/test.sh
+++ typesig_tests/test.sh
@@ -14,7 +14,8 @@ else
 	     orelse1.erl\
 	     product1.erl\
 	     range*.erl\
-	     rec*.erl refined_bug1.erl sequence.erl try*.erl t_union1.erl\
+	     rec*.erl refined_bug1.erl sequence.erl try*.erl spawn4.erl \
+	     t_union1.erl\
 	     tak.erl trecord*.erl tuple*.erl type_widening1.erl"
   ## The following files are currently not giving right results
   #testfiles="guard3.erl record2.erl"
