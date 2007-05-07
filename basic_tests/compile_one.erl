26917269b9c282f3c5a91188795d0e23f6a8b0b5
Per Gustafsson <Per.Gustafsson@it.uu.se>
2007-05-07 15:21:12 +0000

    Added a description and the concurrent_comp option"

--- basic_tests/compile_one.erl
+++ basic_tests/compile_one.erl
@@ -1,3 +1,7 @@
+%%% 2007/05/07 Compiles one function in a module consisting of several
+%%% functions using compile/2 with the concurrent comp option. This
+%%% used to cause the compiler to hang
+ 
 -module(compile_one).
 
 -export([test/0, compile/1]).
@@ -7,7 +11,7 @@ compile(O) ->
   hipe:c(?MODULE, O).
 
 test() ->
-  {ok,{_Platform,_Code}} = hipe:compile({?MODULE, ee, 1}), ok.
+  {ok,{_Platform,_Code}} = hipe:compile({?MODULE, ee, 1}, [concurrent_comp,o2]), ok.
                                                                               
 ae(X) -> if X rem 5 == 0 -> true; true -> false end.
                                                                               
