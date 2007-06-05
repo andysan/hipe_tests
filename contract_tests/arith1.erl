df3ccc0e8f57ccdeaf2ff88a2d8346f1e9150955
Miguel Jimenez García <miji9863>
2007-06-05 19:01:07 +0000

    two more tests added

---
+++ contract_tests/arith1.erl
@@ -0,0 +1,33 @@
+%%%-------------------------------------------------------------------
+%%% File    : arith1.erl
+%%% Author  : Miguel Jimenez <milingo83@gmail.com>
+%%% Description : 
+%%%
+%%% Created : 30 May 2007
+%%%-------------------------------------------------------------------
+-module(arith1).
+-export([add/1, sub/1]).
+
+-spec(add/1::((1) -> integer())). 
+-spec(sub/1::((integer()) -> 3)). 
+
+
+add(X) ->
+  two(X + 1).
+
+two(2) ->
+  2.
+  
+sub(X) ->
+  zero(X - 1).
+
+zero(0) ->
+  0.
+
+  
+
+
+
+
+
+
---
+++ contract_tests/param2.erl
@@ -0,0 +1,43 @@
+%%%-------------------------------------------------------------------
+%%% File    : param2.erl
+%%% Author  : Miguel Jimenez <milingo83@gmail.com>
+%%% Description : Types of the parameter differ
+%%%
+%%% Created : 31 May 2007
+%%%-------------------------------------------------------------------
+-module(param2).
+-export([add/1, mystery/2, mystery2/2]).
+
+%% Will fail
+-spec (add/1::((a()) -> a())). 
+-spec (mul/1 :: ((non_neg_integer()) -> byte())).
+%% Success type for mystery/2:
+%% ((non_neg_integer(),number()) -> byte())
+%% With contract:
+%% ((non_neg_integer(),non_neg_integer()) -> byte())
+%% OK since all are subtypes
+-spec (mystery/2 :: ((a(), a()) -> a())).
+%% Will fail since they are not subtypes
+-spec (mystery2/2 :: ((a(), a()) -> a())).
+
+
+add(X) ->
+  two(X + 1).
+
+two(2) ->
+  ok.
+
+mystery(X, Y)->
+  Z = mul(X),
+  Y + 5.0,
+  Z div 2.
+
+mul(0) -> 0;
+mul(K) -> K*2 + mul(K-1).  
+
+mystery2(X, Y)->
+  mul(X),
+  Y + 5.0.
+
+
+
