6f71bb6a1e3f47bf68a746412eb408b080036e9a
Kostis Sagonas <kostis@cs.ntua.gr>
2007-08-20 18:59:06 +0000

    Cosmetic changes.

--- contract_tests/param2.erl
+++ contract_tests/param2.erl
@@ -13,30 +13,29 @@
 -spec (mystery/2 :: ((B, B) -> B)).
 -spec (mystery2/2 :: ((B, B) -> B)).
 
-
 add(X) ->
   two(X + 1).
 
 two(2) ->
   ok.
 
-mystery(X, Y)->
+mystery(X, Y) ->
   Z = mul(X),
   Y + 5.0,
   Z div 2.
 
 mul(0) -> 0;
-mul(K) -> K*2 + mul(K-1).  
+mul(K) -> (K * 2) + mul(K-1).
 
 mystery2(X, Y) ->
   mul(X),
   Y + 5.0.
 
 mystery2_call() ->
-    mystery2(3, 4).
+  mystery2(3, 4).
 
 mystery2_call2() ->
-    mystery2(3.0, 4.0).
+  mystery2(3.0, 4.0).
 
 mystery2_call3() ->
-    mystery2(3, 4.0).
+  mystery2(3, 4.0).
