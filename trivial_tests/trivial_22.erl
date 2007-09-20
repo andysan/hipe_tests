ba5dc8fd08d0d58a1e1229aaa373add88f46e698
Kostis Sagonas <kostis@cs.ntua.gr>
2007-09-20 15:31:29 +0000

    Rewrote test to avoid R12 compiler warning -- I am afraid I destroyed the intention of the test in the process.

--- trivial_tests/trivial_22.erl
+++ trivial_tests/trivial_22.erl
@@ -1,7 +1,7 @@
 %%%-------------------------------------------------------------------
 %%% File : trivial_22.erl 
 %%% Author : Per Gustafsson <pergu@dhcp-12-245.it.uu.se> 
-%%% Description : Tests that excessive dead code,elimination, which
+%%% Description : Tests that excessive dead code elimination, which
 %%%               breaks the semantics of Erlang, is not performed.
 %%%
 %%% Created :  6 May 2004 by Per Gustafsson <pergu@it.uu.se>
@@ -18,5 +18,8 @@ test() ->
   end.
 
 foo(X) ->
-  X+7,
+  g(X+7),	% was X+7 but the R12 compiler issues a warning
   X.
+
+g(_) ->
+  ok.
