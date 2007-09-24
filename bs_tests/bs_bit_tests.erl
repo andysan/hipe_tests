c76d27ccc8ddbf26df3e842f51beec431f424be4
Per Gustafsson <Per.Gustafsson@it.uu.se>
2007-09-24 11:51:20 +0000

    removed warnings

--- bs_tests/bs_bit_tests.erl
+++ bs_tests/bs_bit_tests.erl
@@ -23,8 +23,8 @@ test() ->
   {ok,ok} = {match1(15),match1(31)},
   ok = horrid_match(),
   ok = test_bitstr(),
-  ok = test_is_bitstr(),
-  ok = test_is_binary(),
+  ok = test_is_bitstr(<<1:1>>,<<8>>),
+  ok = test_is_binary(<<1:1>>,<<8>>),
   ok = test_bitsize(),
   ok = unsymmetric_tests(),
   ok = big_unsymmetric_tests(),
@@ -42,17 +42,17 @@ match1(N) ->
   <<42:N/little>> = <<42:N/little>>,
   ok.
 
-test_is_bitstr() ->
-  true = erlang:is_bitstr(<<1:1>>),
-  true = erlang:is_bitstr(<<8>>),
-  ok = if is_bitstr(<<1:1>>) -> ok end,
-  ok = if is_bitstr(<<8>>) -> ok end.
+test_is_bitstr(Bitstr, Binary) ->
+  true = erlang:is_bitstr(Bitstr),
+  true = erlang:is_bitstr(Binary),
+  ok = if is_bitstr(Bitstr) -> ok end,
+  ok = if is_bitstr(Binary) -> ok end.
 
-test_is_binary() ->
-  false = is_binary(<<1:1>>),
-  true = is_binary(<<8>>),
-  ok = if is_binary(<<1:1>>) -> not_ok; true -> ok end,
-  ok = if is_binary(<<8>>) -> ok end. 
+test_is_binary(Bitstr, Binary) ->
+  false = is_binary(Bitstr),
+  true = is_binary(Binary),
+  ok = if is_binary(Bitstr) -> not_ok; true -> ok end,
+  ok = if is_binary(Binary) -> ok end. 
 
 test_bitsize() ->
   101 = erlang:bitsize(<<1:101>>),
