03503b5cd046ee1f4eb34eabf8d15d785e404ff8
Kostis Sagonas <kostis@cs.ntua.gr>
2007-10-25 20:03:37 +0000

    When will my students learn to put some proper description in test files??

--- basic_tests/hipe_zombie_test.erl
+++ basic_tests/hipe_zombie_test.erl
@@ -1,19 +1,20 @@
-%%%-------------------------------------------------------------------
-%%% File    : test.erl
-%%% Author  : Per Gustafsson <pergu@jobberl>
-%%% Description : 
-%%%
-%%% Created : 25 Oct 2007 by Per Gustafsson <pergu@jobberl>
-%%%-------------------------------------------------------------------
+%%%---------------------------------------------------------------------------------
+%%% File        : hipe_zombie_test.erl
+%%% Author      : Per Gustafsson <pergu@it.uu.se>
+%%% Description : Checks that HiPE's concurrent compilation does not leave any
+%%%		  zombie processes after compilation has finished.
+%%%		  This is a bug reported on erlang-bugs
+%%% Created     : 25 Oct 2007 by Per Gustafsson <pergu@it.uu.se>
+%%%---------------------------------------------------------------------------------
 -module(hipe_zombie_test).
 
 -export([test/0,compile/1]).
 
-compile(O) ->
-  hipe:c(?MODULE,O).
+compile(Opts) ->
+  hipe:c(?MODULE, Opts).
 
 test() ->
   L = length(processes()),
-  hipe:c(?MODULE),
+  hipe:c(?MODULE, [concurrent_comp]),	% force concurrent compilation
   L = length(processes()),
   ok.
