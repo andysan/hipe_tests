0e275a3391b057188a262b4fa39dc198df08af96
Per Gustafsson <Per.Gustafsson@it.uu.se>
2007-11-01 11:44:59 +0000

    new test to generate {bs_save2,Var,{atom,start}}

--- bs_tests/bs_bit_shell_tests.erl
+++ bs_tests/bs_bit_shell_tests.erl
@@ -56,17 +56,17 @@ match1(N) ->
   ok.
 
 test_bitsize() ->
-  101 = parse_and_eval("101 = erlang:bitsize(<<1:101>>)."),
-  1001 = parse_and_eval("1001 = erlang:bitsize(<<1:1001>>)."),
-  80 = parse_and_eval("80 = erlang:bitsize(<<1:80>>)."),
-  800 = parse_and_eval("800 = erlang:bitsize(<<1:800>>)."),
+  101 = parse_and_eval("101 = erlang:bit_size(<<1:101>>)."),
+  1001 = parse_and_eval("1001 = erlang:bit_size(<<1:1001>>)."),
+  80 = parse_and_eval("80 = erlang:bit_size(<<1:80>>)."),
+  800 = parse_and_eval("800 = erlang:bit_size(<<1:800>>)."),
   16#10000001 = parse_and_eval("Bin = <<0:16#1000000>>,
-  BigBin = erlang:list_to_bitstr([Bin||_ <- lists:seq(1,16#10)]++[<<1:1>>]),
-  16#10000001 = erlang:bitsize(BigBin)."),
+  BigBin = erlang:list_to_bitstring([Bin||_ <- lists:seq(1,16#10)]++[<<1:1>>]),
+  16#10000001 = erlang:bit_size(BigBin)."),
   %% Only run these on computers with lots of memory
-  %% HugeBin = erlang:list_to_bitstr([BigBin||_ <- lists:seq(1,16#10)]++[<<1:1>>]),
-  %% 16#100000011 = erlang:bitsize(HugeBin), 
-  0 = parse_and_eval("0 = erlang:bitsize(<<>>)."),
+  %% HugeBin = erlang:list_to_bitstring([BigBin||_ <- lists:seq(1,16#10)]++[<<1:1>>]),
+  %% 16#100000011 = erlang:bit_size(HugeBin), 
+  0 = parse_and_eval("0 = erlang:bit_size(<<>>)."),
   ok.
 
 horrid_match() ->
@@ -104,17 +104,17 @@ big_unsymmetric_tests() ->
   ok.
 
 binary_to_and_from_list() ->
-  <<1:7>> = parse_and_eval("erlang:list_to_bitstr(erlang:bitstr_to_list(<<1:7>>))."),
-  <<1,2,3,4,1:1>> = parse_and_eval("erlang:list_to_bitstr(erlang:bitstr_to_list(<<1,2,3,4,1:1>>))."),
-  [1,2,3,4,<<1:1>>] = parse_and_eval("erlang:bitstr_to_list(<<1,2,3,4,1:1>>)."),
-  <<1:1,1,2,3,4>> = parse_and_eval("erlang:list_to_bitstr([<<1:1>>,1,2,3,4])."),
-  [128,129,1,130,<<0:1>>] = parse_and_eval("erlang:bitstr_to_list(<<1:1,1,2,3,4>>)."),
+  <<1:7>> = parse_and_eval("erlang:list_to_bitstring(erlang:bitstring_to_list(<<1:7>>))."),
+  <<1,2,3,4,1:1>> = parse_and_eval("erlang:list_to_bitstring(erlang:bitstring_to_list(<<1,2,3,4,1:1>>))."),
+  [1,2,3,4,<<1:1>>] = parse_and_eval("erlang:bitstring_to_list(<<1,2,3,4,1:1>>)."),
+  <<1:1,1,2,3,4>> = parse_and_eval("erlang:list_to_bitstring([<<1:1>>,1,2,3,4])."),
+  [128,129,1,130,<<0:1>>] = parse_and_eval("erlang:bitstring_to_list(<<1:1,1,2,3,4>>)."),
   ok.
  
 big_binary_to_and_from_list() ->
-  <<1:800,2,3,4,1:1>> = parse_and_eval("erlang:list_to_bitstr(erlang:bitstr_to_list(<<1:800,2,3,4,1:1>>))."),
-  [1,2,3,4|_Rest1] = parse_and_eval("erlang:bitstr_to_list(<<1,2,3,4,1:800,1:1>>)."),
-  <<1:801,1,2,3,4>> = parse_and_eval("erlang:list_to_bitstr([<<1:801>>,1,2,3,4])."),
+  <<1:800,2,3,4,1:1>> = parse_and_eval("erlang:list_to_bitstring(erlang:bitstring_to_list(<<1:800,2,3,4,1:1>>))."),
+  [1,2,3,4|_Rest1] = parse_and_eval("erlang:bitstring_to_list(<<1,2,3,4,1:800,1:1>>)."),
+  <<1:801,1,2,3,4>> = parse_and_eval("erlang:list_to_bitstring([<<1:801>>,1,2,3,4])."),
   ok.  
 
 send_and_receive() -> 
