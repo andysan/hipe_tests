b549d36b603248dd9f0b8976f410f111b32c8c97
Daniel Luna <luna@update.uu.se>
2008-03-12 11:10:07 +0000

    Test minimized.

--- checker_tests/different_types_sizes_bug_01.erl
+++ checker_tests/different_types_sizes_bug_01.erl
@@ -9,33 +9,10 @@
 -module(different_types_sizes_bug_01).
 -export([test/0]).
 
+%% Code minimized from code_server:do_load_binary/4
 test() ->
-  case {atom, atom} of
-    {atom, atom} -> ok
+  case {true()} of
+    {true} -> ok
   end.
 
-do_load_binary(Module,File,Binary,Db) ->
-    case {modp(Module),modp(File)} of
-	{true, true} when is_binary(Binary) ->
-	    case erlang:module_loaded(code_aux:to_atom(Module)) of
-		true ->
-		    code_aux:do_purge(Module);
-		false ->
-		    ok
-	    end,
-	    try_load_module(File, Module, Binary, Db);
-	_ ->
-	    {error, badarg}
-    end.
-
-modp(Atom) when is_atom(Atom) -> true;
-modp(List) when is_list(List) -> int_list(List);
-modp(_)                       -> false.
-
-int_list([H|T]) when is_integer(H) -> int_list(T);
-int_list([_|_])                    -> false;
-int_list([])                       -> true.
-
-try_load_module(_, _, _, _) -> ok.
-
-%%  catch code_server:a().
+true() -> true.
