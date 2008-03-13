031568c34302485b11b0f3712b70fb43dd5e3ab8
Daniel Luna <luna@update.uu.se>
2008-03-13 11:06:45 +0000

    Removed printout from tests.

--- checker_tests/fail_01.erl
+++ checker_tests/fail_01.erl
@@ -11,7 +11,7 @@
 
 -spec(test/0 :: () -> 'ok').
 test() ->
-  io:format(atom_to_list(?MODULE) ++ " running~n"),
+  %% io:format(atom_to_list(?MODULE) ++ " running~n"),
   two(3),
   takes_atom(5),
   ko.
