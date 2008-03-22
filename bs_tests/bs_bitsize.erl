95f1a099ba4569fb6e5f44a338b1d3fabab31ab6
Kostis Sagonas <kostis@cs.ntua.gr>
2008-03-22 09:41:18 +0000

    Added one more function which tests constant propagation & the bit_size/1 test.

--- bs_tests/bs_bitsize.erl
+++ bs_tests/bs_bitsize.erl
@@ -6,6 +6,7 @@
 test() ->
   true = bitsize_in_body(<<1:42>>),
   true = bitsize_in_guard(<<1:7>>),
+  8 = constant_binary(42),
   ok.
 
 bitsize_in_body(Bin) ->
@@ -15,6 +16,9 @@ bitsize_in_guard(Bin) when erlang:bit_size(Bin) rem 7 =:= 0 ->
   true;
 bitsize_in_guard(Bin) when is_binary(Bin) ->
   false.
+
+constant_binary(N) when N > 0 ->
+  bit_size(<<42>>).
 
 compile(Opts) ->
   hipe:c(?MODULE, Opts).
