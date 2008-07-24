850b2a311f9ce883b620ebba0c1d18a94a076031
Kostis Sagonas <kostis@cs.ntua.gr>
2008-07-24 16:53:30 +0000

    Fixed contract.

--- checker_tests/dialyzer_01.erl
+++ checker_tests/dialyzer_01.erl
@@ -1,11 +1,16 @@
 -module(dialyzer_01).
 -export([test/0]).
 
--spec(test/0 :: () -> {'ok',[_]}).
+-define(QUICK_TEST, true).
+
+-spec test() -> [_].
+
+-ifdef(QUICK_TEST).
 test() ->
   Files = filelib:wildcard("di*.erl"),
   dialyzer:run([{files,Files},{from,src_code}]).
-
-%% test() ->
-%%   Ebins = filelib:wildcard(filename:join(code:lib_dir(), "typer/ebin")),
-%%   dialyzer:run([{files,Ebins}]).
+-else.	%% this really takes long time
+test() ->
+  Ebins = filelib:wildcard(filename:join(code:lib_dir(), "typer/ebin")),
+  dialyzer:run([{files,Ebins}]).
+-endif.
